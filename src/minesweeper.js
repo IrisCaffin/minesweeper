709faa0a931b68e9b4404250d33e6a0e7fb69ce9
iriscaffin <iriscaffin@gmail.com>
2017-09-17 14:06:39 -0700

    Update minesweeper.js

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -1,11 +1,11 @@
-var blankLine = "  |   |  ";
+const blankLine = "  |   |  ";
 console.log("This is what an empty board would look like:");
 console.log(blankLine);
 console.log(blankLine);
 console.log(blankLine);
 
-var guessLine = "1 |   |  ";
-var bombLine = "  | B |  ";
+const guessLine = "1 |   |  ";
+const bombLine = "  | B |  ";
 
 console.log("This is what a board with a guess and a bomb on it would look like");
 console.log(guessLine);
